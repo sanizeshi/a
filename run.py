de83a0fc6bf268cbf49c8421b8917a851b6b9746
sanizeshi <82694248+sanizeshi@users.noreply.github.com>
2021-07-11 19:18:09 +0500

    Delete run.py

--- run.py
+++
@@ -1,38 +0,0 @@
-�
-g�`c@s�y�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
-Z
-ddlZddlZddl
-mZWn+ek
-r�ejd�ejd�nXejd�ee�ejd�dZdZd	Zd
-�ZdZd�Zd
-GHdZdZdZxyedkr�ed�Zeekr�ed�Zeekr�deGHdZq�dGHejd�qFdGHejd�qFWejd�dS(i����N(t
-ThreadPoolspip2 install requestsspython2 index.xbntclearsutf-8s[1;32ms[0;97ms[1;31mcCsCx<|dD]0}tjj|�tjj�tjd�qWdS(Ns
-g���MbP?(tsyststdouttwritetflushttimetsleep(tzte((s(/storage/emulated/0/Download/Ram/ROCK.PYtjalans
-tcCstjd�dS(NR(tostsystem(((s(/storage/emulated/0/Download/Ram/ROCK.PYtregss�[1;97m
-[1;97m   ########      ###     ########      ###    
-[1;97m   ##     ##    ## ##    ##     ##    ## ##   
-[1;93m   ##     ##   ##   ##   ##     ##   ##   ##  
-[1;93m   ########   ##     ##  ########   ##     ## 
-[1;93m   ##     ##  #########  ##     ##  ######### 
-[1;97m   ##     ##  ##     ##  ##     ##  ##     ## 
-[1;97m   ########   ##     ##  ########   ##     ## 
-[1;93m-----------------------------------------------
-[1;97m     ║║[1;41m[->  UJ<-][1;0m║║
-[1;93m-----------------------------------------------
-[1;97m╰╼➣ AUTHOR     : UN
-[1;97m╰╼➣ HAHA   : NAM TU SUNA HUGA
-[1;97m╰╼➣ Version  : 3.0
-[1;97m╰╼➣ UN : UN
-[1;93m-----------------------------------------------
-[1;97m ║║[1;41m[->  THIS TOOL IS FREE NOT FOR SALE  <-][1;0m║║
-[1;93m-----------------------------------------------tHACKERttrues6[1;92m«««---~[1;92m(Username)[1;92m~----»»»  s6[1;92m«««---~[1;92m(Password)[1;92m~----»»»  sLogged in successfully as tfalsesWrong password!s+xdg-open https://m.facebook.com/H4CK3R.BABAsWrong username!spython2 Baba-Compiler.py(RRRtdatetimetretrandomthashlibt	threadingtjsontgetpassturllibt	cookielibtrequeststmultiprocessing.poolRtImportErrorR
-treloadtsetdefaultencodingtctc2tc3R
-tlogoRtCorrectUsernametCorrectPasswordtloopt	raw_inputtusernametpassword(((s(/storage/emulated/0/Download/Ram/ROCK.PYt<module>s<�
-
-
-
-
-				
- 
- 
-	1
